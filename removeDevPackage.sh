6224cf28a93c0f95178082dcfc54946ac26da1a0
Ben Rollin <ben.rollin@clever.com>
2024-02-12 11:22:27 -0800

    add remove package script

---
+++ removeDevPackage.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Removing mod..."
+rm '/Users/ben.rollin/Library/Application Support/Steam/steamapps/common/SlayTheSpire/SlayTheSpire.app/Contents/Resources/mods/SpeakTheSpire.jar'
