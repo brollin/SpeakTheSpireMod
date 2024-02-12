8ef0716e67195efde9eb8e426db9f1c9c82bdece
Ben Rollin <ben.rollin@clever.com>
2024-02-12 01:46:32 -0800

    v0.2.1

--- packageAndPublish.sh
+++ packageAndPublish.sh
@@ -12,3 +12,6 @@ echo "Uploading mod to steam workshop..."
 cp -r ./steam/* '/Users/ben.rollin/Library/Application Support/Steam/steamapps/common/SlayTheSpire/SlayTheSpire.app/Contents/Resources/speakthespire/'
 cd '/Users/ben.rollin/Library/Application Support/Steam/steamapps/common/SlayTheSpire/SlayTheSpire.app/Contents/Resources'
 ./jre/bin/java -jar mod-uploader.jar upload -w speakthespire
+
+# Note: before the above upload command will work, you need to have created a workspace with the following command:
+# ./jre/bin/java -jar mod-uploader.jar new -w speakthespire
